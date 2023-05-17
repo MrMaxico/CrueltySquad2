d9a78f045da2d7868f38b21f624ac3814f4a60e5
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-05-17 15:45:42 +0200

    pickup script

--- CrueltySquad2/Assets/Scripts/Arthur's test scripts/PickUpController.cs
+++ CrueltySquad2/Assets/Scripts/Arthur's test scripts/PickUpController.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUpController : MonoBehaviour
-{
-    public GameObject primaryHolder;
+public class PickUpController : MonoBehaviour {
+    public Transform primaryHolder;
     public GameObject secondaryHolder;
+    public GameObject cam;
     public RaycastHit hit;
+    public bool holdingPrimary;
+    public bool holdingSecondary;
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKey(KeyCode.E)) {
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 1000)) {
-                
-                print(hit.collider.gameObject.tag);
+            Debug.Log("Pressed E");
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 1000)) {
+                if (hit.transform.CompareTag("Gun")) {
+                    Debug.Log("Gun");
+                    PickUpGun(hit.transform);
+                }
             }
         }
+    }
+
+    void PickUpGun(Transform gunTransform) {
+        gunTransform.SetParent(primaryHolder);
+        gunTransform.position = primaryHolder.position;
+        Rigidbody gunRigidbody = gunTransform.GetComponent<Rigidbody>();
+        gunRigidbody.useGravity = false;
+        gunRigidbody.freezeRotation = true;
+        gunRigidbody.isKinematic = true;
+        // Additional gun pickup logic if needed
     }
 }
