fd7f423eac4c0dc1c3fd371fbe1349114da6145a
MrMaxico <mr.maxico4@gmail.com>
2023-05-22 13:08:35 +0200

    meer randomness in enemy sounds

--- CrueltySquad2/Assets/Scripts/max zn zooi/EnemySounds.cs
+++ CrueltySquad2/Assets/Scripts/max zn zooi/EnemySounds.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnemySounds : MonoBehaviour
 {
-    public AudioSource monsterAttack;
-    
+    public AudioSource [] monsterAttack;
+
+
     void SoundAttack()
     {
-        monsterAttack.Play();
-        monsterAttack.pitch = Random.Range(0.7f, 1.3f);
+        int index = Random.Range(0, monsterAttack.Length);
+        monsterAttack[index].Play();
+        monsterAttack[index].pitch = Random.Range(0.7f, 1.3f);
     }
 }
