20c1509bc72954133a92659916d80804a0418d12
MrMaxico <mr.maxico4@gmail.com>
2023-05-26 13:41:08 +0200

    crawler anims en logbook update

--- CrueltySquad2/Assets/Scripts/max zn zooi/EnemySounds.cs
+++ CrueltySquad2/Assets/Scripts/max zn zooi/EnemySounds.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemySounds : MonoBehaviour
 {
     public AudioSource [] monsterAttack;
+    public AudioSource [] monsterAttack2;
+    public AudioSource [] walking;
+
 
 
     void SoundAttack()
@@ -12,5 +15,19 @@ public class EnemySounds : MonoBehaviour
         int index = Random.Range(0, monsterAttack.Length);
         monsterAttack[index].Play();
         monsterAttack[index].pitch = Random.Range(0.7f, 1.3f);
+    }
+
+    void SoundAttack2()
+    {
+        int index = Random.Range(0, monsterAttack.Length);
+        monsterAttack2[index].Play();
+        monsterAttack2[index].pitch = Random.Range(0.7f, 1.3f);
+    }
+
+    void SoundWalking()
+    {
+        int index = Random.Range(0, monsterAttack.Length);
+        walking[index].Play();
+        walking[index].pitch = Random.Range(0.5f, 0.8f);
     }
 }
