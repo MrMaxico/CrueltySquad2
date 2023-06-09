739b7757d08f0fefcb4fbe2606c8de8b775e448d
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-06-09 16:36:18 +0200

    gun stats aan het fixen

--- CrueltySquad2/Assets/Scripts/Arthur's test scripts/GunData.cs
+++ CrueltySquad2/Assets/Scripts/Arthur's test scripts/GunData.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public enum GunType {
     Pistol,
@@ -16,8 +17,25 @@ public class GunData : MonoBehaviour {
     public float range = 100;
     public float reloadspeed = 1f;
     public GunType gunType = GunType.Pistol;
+    public GameObject gunStatUI;
     [Header("Settings for shotguns", order = 0)]
     public float shotgunSpreadAngle = 15f;
     public float shotgunPelletCount = 15f;
     // Other gun-related variables and functions can be added here
+
+    public void Start() {
+        gunStatUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = gameObject.name; //Weapon name
+        gunStatUI.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "lvl: 1".ToString(); //make Level of gun
+        gunStatUI.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = " "; //make modifier name
+        if (gunType == GunType.Shotgun) {
+            int result = 5 * 15;
+            gunStatUI.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = result.ToString();
+        }
+        gunStatUI.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = damagePerBullet.ToString(); //damage
+        gunStatUI.transform.GetChild(4).GetComponent<TextMeshProUGUI>().text = reloadspeed.ToString(); //reload speed
+        gunStatUI.transform.GetChild(5).GetComponent<TextMeshProUGUI>().text = magSize.ToString(); //magSize
+        gunStatUI.transform.GetChild(6).GetComponent<TextMeshProUGUI>().text = fireRate.ToString(); //FireRate
+        gunStatUI.transform.GetChild(7).GetComponent<TextMeshProUGUI>().text = range.ToString(); //Range
+
+    }
 }
