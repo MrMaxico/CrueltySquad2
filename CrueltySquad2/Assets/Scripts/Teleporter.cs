05e8de1fb7ff2a178263b799f1ced70a3d339331
NickSwitchSchool <97087180@st.deltion.nl>
2023-06-20 16:16:43 +0200

    island number

--- CrueltySquad2/Assets/Scripts/Teleporter.cs
+++ CrueltySquad2/Assets/Scripts/Teleporter.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Teleporter : MonoBehaviour
 {
+    static public int islandNumber; 
     public int spawnersLeft;
     bool open;
 
+    private void Start()
+    {
+        if (islandNumber < 1)
+        {
+            islandNumber = 1;
+        }
+    }
     private void Update()
     {
         if (spawnersLeft < 1)
@@ -30,6 +38,7 @@ public class Teleporter : MonoBehaviour
     {
         if (open)
         {
+            islandNumber++;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
