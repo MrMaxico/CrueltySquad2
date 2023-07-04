9d281579d1135b49b7f1fd055822cbb9a2df049d
NickSwitchSchool <97087180@st.deltion.nl>
2023-07-04 15:53:14 +0200

    objective UI

---
+++ CrueltySquad2/Assets/Scripts/ObjectiveUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectiveUI : MonoBehaviour
+{
+    public TextMeshProUGUI islandNumber;
+    public TextMeshProUGUI objectiveOne;
+    public TextMeshProUGUI objectiveTwo;
+    GameObject teleporter;
+
+    private void Start()
+    {
+        islandNumber.text = $"Island: {Teleporter.islandNumber}";
+    }
+
+    public void Update()
+    {
+        if (teleporter == null)
+        {
+            teleporter = GameObject.FindGameObjectWithTag("Teleporter");
+        }
+        else if (teleporter.GetComponent<Teleporter>().spawnersLeft > 0)
+        {
+            objectiveOne.text = $"Destroy all bug nests";
+            objectiveTwo.text = $"Bug nests left: {teleporter.GetComponent<Teleporter>().spawnersLeft}";
+        }
+        else if (teleporter.GetComponent<Teleporter>().spawnersLeft <= 0)
+        {
+            objectiveOne.text = $"Find and activate the teleporter";
+            objectiveTwo.text = $"";
+        }
+    }
+}
