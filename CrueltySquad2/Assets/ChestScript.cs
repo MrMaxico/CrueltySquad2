e0116e89e74337b396055ead93a2c28f8d61d12a
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-07-05 21:54:34 +0200

    chest werkt

--- CrueltySquad2/Assets/ChestScript.cs
+++ CrueltySquad2/Assets/ChestScript.cs
@@ -7,17 +7,39 @@ public class ChestScript : MonoBehaviour {
     public WeightedRandomList<GameObject> lootTable;
     public Transform gunSpawnLocation;
     private float cycleSpeed = 1;
+    public float amountOfCycles = 10;
     private bool waiting;
+    private bool Delay = true;
+    public GameObject weapon;
+    public GameObject prevWeapon;
+    private int i;
+    public Animator chestAnim;
+    public float chestAnimDelay;
     public void Open() {
-        for (int i = 0; !waiting && i < 10; i++) {
+        if(i <= amountOfCycles && waiting == false) {
             waiting = true;
-            WeaponCycle();
+            StartCoroutine(WeaponCycle());
+        } else if(waiting == false) {
+            weapon = GameObject.Instantiate(lootTable.GetRandom(), gunSpawnLocation.position, gunSpawnLocation.rotation);
+            weapon.transform.parent = gunSpawnLocation;
+            waiting = true;
         }
     }
     private IEnumerator WeaponCycle() {
+        if (i <= 0 && Delay) {
+            chestAnim.SetTrigger("open");
+            yield return new WaitForSeconds(chestAnimDelay);
+            Delay = false;
+        }
+        prevWeapon = weapon;
+        weapon = GameObject.Instantiate(lootTable.GetRandom(), gunSpawnLocation.position, gunSpawnLocation.rotation);
+        weapon.GetComponent<Rigidbody>().useGravity = false;
+        weapon.GetComponent<Collider>().enabled = false;
+        weapon.GetComponent<Rigidbody>().isKinematic = false;
         yield return new WaitForSeconds(cycleSpeed);
-        GameObject.Instantiate(lootTable.GetRandom(), transform.position, transform.rotation).GetComponent<Collider>().enabled = false;
-        cycleSpeed -= 0.1f;
+        Destroy(weapon);
+        i++;
         waiting = false;
+        Open();
     }
 }
